e471759e6af35217ced76a58768933d62d0a01b9
Vetrov Vadim <vetrovvd@yandex.ru>
2022-07-06 22:08:28 +0300

    about me change

--- src/views/RootView.vue
+++ src/views/RootView.vue
@@ -5,8 +5,8 @@
     <div class="about">
       Hello there and welcome to my personal website!<br /><br />
       My name is Waujito. Real name is Vetrov Vadim.<br />
-      I am a programer. It is a really fun and cool hobby for me. I love it
-      💖<br />
+      I am a programer. Programming is a really fun and cool hobby for me. I
+      love it 💖<br />
       I'm currently learning programming languages such as Python, PHP(with
       Laravel), JavaScript(with Vue.js)
     </div>
