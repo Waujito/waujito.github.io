36498cae97507703259a83a00baa08434f60de6e
Vetrov Vadim <vetrovvd@yandex.ru>
2022-07-07 16:41:06 +0300

    nav animations

--- src/components/NavPanelComponent.vue
+++ src/components/NavPanelComponent.vue
@@ -4,31 +4,37 @@ import { RouterLink } from "vue-router";
 
 <template>
   <nav class="leftside_nav">
-    <RouterLink to="/" class="main_router_link">
-      <div>
-        <img src="/img/home.svg" alt="Home" />
-        <span class="alternative_text">Home</span>
-      </div>
-    </RouterLink>
-    <RouterLink to="/projects" class="main_router_link">
-      <div>
-        <img
-          src="/img/projects.svg"
-          alt="Projects"
-          style="width: 85%; height: 85%"
-        />
-        <span class="alternative_text">Projects</span>
-      </div>
-    </RouterLink>
-    <RouterLink to="/contact" class="main_router_link">
-      <div>
-        <img
-          src="/img/contact_me.svg"
-          alt="Contact me"
-          style="width: 85%; height: 85%"
-        />
-        <span class="alternative_text">Contact me</span>
-      </div>
-    </RouterLink>
+    <div class="nav_link">
+      <RouterLink to="/" class="main_router_link">
+        <div>
+          <img src="/img/home.svg" alt="Home" />
+          <span class="alternative_text">Home</span>
+        </div>
+      </RouterLink>
+    </div>
+    <div class="nav_link">
+      <RouterLink to="/projects" class="main_router_link">
+        <div>
+          <img
+            src="/img/projects.svg"
+            alt="Projects"
+            style="width: 85%; height: 85%"
+          />
+          <span class="alternative_text">Projects</span>
+        </div>
+      </RouterLink>
+    </div>
+    <div class="nav_link">
+      <RouterLink to="/contact" class="main_router_link">
+        <div>
+          <img
+            src="/img/contact_me.svg"
+            alt="Contact me"
+            style="width: 85%; height: 85%"
+          />
+          <span class="alternative_text"><nobr>Contact me</nobr></span>
+        </div>
+      </RouterLink>
+    </div>
   </nav>
 </template>
