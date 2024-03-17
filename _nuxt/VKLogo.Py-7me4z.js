40f3874fe4d8d446faad495fa606685014359bed
Waujito <vetrovvd@gmail.com>
2024-03-17 06:39:10 +0000

    Deploying to gh-pages from @ Waujito/waujito.github.io@d97341269ffc6b4106218eea4c82b5e04fef82d3 🚀

---
+++ _nuxt/VKLogo.Py-7me4z.js
@@ -0,0 +1,1 @@
+import"./entry.CIADgTIt.js";const o=""+globalThis.__publicAssetsURL("img/telegram.svg"),i=""+globalThis.__publicAssetsURL("img/VKLogo.svg");export{o as _,i as a};
---
+++ _nuxt/github.Ca7luGDy.js
@@ -0,0 +1,1 @@
+import"./entry.CIADgTIt.js";const i=""+globalThis.__publicAssetsURL("img/github.png");export{i as _};
